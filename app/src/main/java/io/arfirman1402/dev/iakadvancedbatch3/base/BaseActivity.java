882fb79b04f66b8f350d8e789443a037587f43e9
arfirman1402 <ar.firman.syahputra@gmail.com>
2017-05-14 11:15:40 +0700

    Reset MainActivity and using BaseActivity

--- app/src/main/java/io/arfirman1402/dev/iakadvancedbatch3/base/BaseActivity.java
+++ app/src/main/java/io/arfirman1402/dev/iakadvancedbatch3/base/BaseActivity.java
@@ -5,12 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import butterknife.ButterKnife;
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
 
 /**
  * Created by alodokter-it on 13/05/17 -- BaseActivity.
  */
 
 public class BaseActivity extends AppCompatActivity {
+    private Subscription subscriber = new CompositeSubscription();
+
     protected void bind(int layout) {
         setContentView(layout);
         ButterKnife.bind(this);
@@ -24,5 +28,11 @@ public class BaseActivity extends AppCompatActivity {
         Intent i = new Intent(this, dest);
         startActivity(i);
         finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        subscriber.unsubscribe();
     }
 }
--- app/src/main/java/io/arfirman1402/dev/iakadvancedbatch3/view/main/MainActivity.java
+++ app/src/main/java/io/arfirman1402/dev/iakadvancedbatch3/view/main/MainActivity.java
@@ -1,100 +1,15 @@
 package io.arfirman1402.dev.iakadvancedbatch3.view.main;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import io.arfirman1402.dev.iakadvancedbatch3.R;
-import io.arfirman1402.dev.iakadvancedbatch3.model.DataBean;
-import rx.Observable;
-import rx.Subscription;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
-import rx.subscriptions.CompositeSubscription;
+import io.arfirman1402.dev.iakadvancedbatch3.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.txt_test)
-    TextView txtTest;
-
-    private Subscription subscriber = new CompositeSubscription();
-    private String TAG = this.getClass().getSimpleName();
-    private ArrayList<String> dataString = new ArrayList<>();
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-
-        prosesData();
-        prosesData2();
-    }
-
-    private void prosesData2() {
-        DataBean identitas = new DataBean("Ar Firman Syahputra", 22);
-        Observable<DataBean> dataBeanObs = Observable.just(identitas);
-
-        subscriber = dataBeanObs.observeOn(Schedulers.io()).subscribeOn(AndroidSchedulers.mainThread()).subscribe(new ObsSub2()::onSuccess, new ObsSub2()::onError, new ObsSub2()::onCompleted);
-    }
-
-    private void prosesData() {
-        /*Observable<String[]> dataStr = Observable.just(new String[]{"Indonesia", "Raya"}, new String[]{"Merdeka", "Merdeka"});
-
-        subscriber = dataStr.map(strings -> {
-            StringBuilder sb = new StringBuilder();
-            for (String s : strings) {
-                sb.append(s + " ");
-            }
-            return sb.toString();
-        }).observeOn(Schedulers.io())
-                .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe(new ObserverMain()::onSuccess, new ObserverMain()::onError, new ObserverMain()::onCompleted);*/
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        subscriber.unsubscribe();
-    }
-
-    private class ObserverMain {
-        private void onSuccess(String s) {
-            Log.d(TAG, "onNext: " + s);
-            dataString.add(s);
-            txtTest.setText(s);
-        }
-
-        private void onError(Throwable e) {
-            Log.e(TAG, "onError: " + e.toString());
-        }
-
-        private void onCompleted() {
-            Log.d(TAG, "onCompleted: Has Reached");
-            StringBuilder sb = new StringBuilder();
-            for (String s : dataString) {
-                sb.append(s);
-            }
-            txtTest.setText(sb.toString());
-        }
-    }
-
-    private class ObsSub2 {
-        private void onSuccess(DataBean dataBean) {
-            txtTest.setText(dataBean.getNama() + " berumur " + dataBean.getUmur());
-        }
-
-        private void onError(Throwable e) {
-            Log.e(TAG, "onError: " + e.toString());
-        }
-
-        private void onCompleted() {
-            Log.d(TAG, "onCompleted: Has Reached");
-        }
+        bind(R.layout.activity_main);
     }
 }
